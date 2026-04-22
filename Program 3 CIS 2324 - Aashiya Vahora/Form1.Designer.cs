30b2beb4cf002428acf6856786ec2ec0536bb6c5
AashiyaV <138508075+AashiyaV@users.noreply.github.com>
2026-04-21 19:55:29 -0500

    Add files via upload

---
+++ Program 3 CIS 2324 - Aashiya Vahora/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Program_3_CIS_2324___Aashiya_Vahora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(709, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Cost";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(589, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Location";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(476, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Seminar";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(554, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 52);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Exit Application";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 52);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 52);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Clear Selections & Output Box";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 52);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(422, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Total Cost To-Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Location";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Seminar";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(450, 91);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(333, 95);
+            this.listBox3.TabIndex = 16;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.listBox2.Location = new System.Drawing.Point(254, 91);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(153, 82);
+            this.listBox2.TabIndex = 15;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Mangement",
+            "Supervision Skills",
+            "Negotiation",
+            "How to Interview"});
+            this.listBox1.Location = new System.Drawing.Point(18, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 82);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Program 3 CIS 2324 - Aashiya Vahora/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_3_CIS_2324___Aashiya_Vahora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Variable Decleration
+        string Seminar = " ", LocationSelected = " ", DisplayData = " ";
+        double SeminarCost = 0, LocationCost = 0, NumDays = 0, TotalCost = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessSeminar();
+            ProcessLocation();
+            CalculateTotal();
+            DisplayOutput();
+        }
+        private void ProcessSeminar()
+        {
+            Seminar = listBox1.SelectedItem.ToString();
+            if (Seminar == ("Handling Stress"))
+            {
+                SeminarCost = 1000;
+                NumDays = 3;
+            }
+            else if (Seminar == "Time Mangement")
+            {
+                SeminarCost = 800;
+                NumDays = 3;
+            }
+            else if (Seminar == "Supervision Skills")
+            {
+                SeminarCost = 1500;
+                NumDays = 3;
+            }
+            else if (Seminar == "Negotiation")
+            {
+                SeminarCost = 1300;
+                NumDays = 5;
+            }
+            else if (Seminar == "How to Interview")
+            {
+                SeminarCost = 500;
+                NumDays = 1;
+            }
+        }
+        private void ProcessLocation()
+        {
+            LocationSelected = listBox2.SelectedItem.ToString();
+            if (LocationSelected == "Austin")
+            { LocationCost = 150; }
+            else if (LocationSelected == "Chicago")
+            { LocationCost = 225; }
+            else if (LocationSelected == "Dallas")
+            { LocationCost = 175; }
+            else if (LocationSelected == "Orlando")
+            { LocationCost = 300; }
+            else if (LocationSelected == "Phoenix")
+            { LocationCost = 175; }
+            else if (LocationSelected == "Raleigh")
+            { LocationCost = 150; }
+        }
+        private void CalculateTotal()
+        {
+            TotalCost = (SeminarCost + (LocationCost * NumDays));
+        }
+        private void DisplayOutput()
+        {
+            label4.Text = TotalCost.ToString("C");
+            DisplayData = (listBox1.SelectedItem.ToString() + "                       " + listBox2.SelectedItem.ToString() +
+                "                       " + TotalCost.ToString("C"));
+            listBox3.Items.Add(DisplayData);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItem = "";
+            listBox1.SelectedIndex = -1;
+            listBox2.SelectedItem = "";
+            listBox2.SelectedIndex = -1;
+            listBox3.SelectedItem = "";
+            listBox3.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItem = "";
+            listBox1.SelectedIndex = -1;
+            listBox2.SelectedItem = "";
+            listBox2.SelectedIndex = -1;
+            listBox3.SelectedItem = "";
+            listBox3.Items.Clear();
+            label4.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("About to terminate app");
+            Application.Exit();
+        }
+    }
+}
